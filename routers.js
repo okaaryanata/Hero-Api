0da3ab268325f8ec2eeec0dc05ccc7106f011ed8
A.A. Gede Oka Aryanata 37 <oka.aryanata9@gmail.com>
2019-06-06 04:30:35 +0800

    finishing another http method

--- routers.js
+++ routers.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const Heroes = require('./models/heroes');
 
 router.get('/heroes',function(req,res){
-    res.send('GET Heroes')
-})
+    Heroes.find({}).then(function(result){
+        res.send(result);
+    });
+});
+
+router.get('/heroes/:id',function(req,res){
+    Heroes.findOne({_id:req.params.id}).then(function(result){
+        res.send(result);
+    });
+});
 
 router.post('/heroes',function(req,res, next){
     let {name, role} = req.body;
@@ -18,11 +26,17 @@ router.post('/heroes',function(req,res, next){
 });
 
 router.put('/heroes/:id',function(req,res){
-    res.send('PUT Heroes')
-})
+    Heroes.findOneAndUpdate({_id : req.params.id},req.body).then(function(result){
+        Heroes.findOne({_id : req.params.id}).then(function(result){
+            res.send(result);
+        });
+    });
+});
 
 router.delete('/heroes/:id',function(req,res){
-    res.send('DELETE Heroes')
-})
+    Heroes.findOneAndRemove({_id : req.params.id}).then(function(result){
+        res.send(result);
+    });
+});
 
 module.exports = router;
